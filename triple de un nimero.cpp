d8d8b6a1e5c95d074948dc104a089464c8e7afb7
OCEGUERA DANIEL <60414657+OCEGUERADANIEL@users.noreply.github.com>
2020-02-04 17:33:23 -0800

    Create triple de un nimero.cpp

---
+++ triple de un nimero.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ 
+int main () 
+{
+    int numero;
+    std::cout << "Dime un número: ";
+    std::cin >> numero;
+    std::cout << "El triple de tu número es " << numero*3;
+    return 0;
+}
